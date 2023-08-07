069c0e2e8792a69ac4b6a6d5eaed7f144a7e704b
Abhinav Jha <abhinavjha1947@gmail.com>
2023-08-07 06:06:15 -0700

    reset button update

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,18 +6,41 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
-  user = { email: '' };
+  user = {
+    email: '',
+    newPassword: '',
+    confirmPassword: ''
+  };
 
-  constructor(private userService: UserService) {}
+  otp: string = '';
+  showOtpForm: boolean = false;
+  showResetForm: boolean = false;
+
+  requestResetPassword() {
+    // Placeholder logic for demonstration purposes
+    // You can implement the logic to generate and send the OTP to the user's email here
+    console.log('Requesting reset password for email:', this.user.email);
+
+    // Simulate successful OTP request, show the OTP form
+    this.showOtpForm = true;
+  }
+
+  verifyOtp() {
+    // Placeholder logic for demonstration purposes
+    // You can implement the logic to verify the OTP entered by the user here
+    console.log('Verifying OTP:', this.otp);
+
+    // Simulate successful OTP verification, show the password reset form
+    this.showResetForm = true;
+    this.showOtpForm = false;
+  }
 
   resetPassword() {
-    this.userService.resetPassword(this.user.email).subscribe(
-      (response) => {
-        console.log('Password reset link sent successfully');
-      },
-      (error) => {
-        console.error('Password reset request failed:', error);
-      }
-    );
+    // Placeholder logic for demonstration purposes
+    // You can implement the logic to reset the user's password here
+    console.log('Resetting password for email:', this.user.email);
+
+    // Simulate successful password reset, navigate the user back to the login page
+    // You can use Angular's router to navigate back to the login page
   }
 }
