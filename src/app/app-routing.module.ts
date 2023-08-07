2fcd4a25dab7e704eceba8caac43182961b75cb0
Nirva <padaliyanirva11@gmail.com>
2023-08-07 19:03:08 +0530

    Dayend commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NewResearchComponent} from '../app/new-research/new-research.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'new-research', component: NewResearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResearchBookComponent } from './research-book/research-book.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResearchDialogBoxComponent } from './research-dialog-box/research-dialog-box.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NewResearchComponent } from './new-research/new-research.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResearchBookComponent
+    ResearchBookComponent,
+    ResearchDialogBoxComponent,
+    NewResearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
